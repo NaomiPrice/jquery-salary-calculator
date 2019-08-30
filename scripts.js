36f21c1a76f24677bb5de74a13b7a49195b34bd7
Naomi Price <naomi.price71@gmail.com>
2019-08-30 17:44:09 -0500

    display employee function working

--- scripts.js
+++ scripts.js
@@ -8,13 +8,14 @@ function onReady(){
 let employeeList = [];
 
 function addEmployee(){
+    //get user input
     console.log('button clicked');
     let firstName = $('#firstName').val();
     let lastName = $('#lastName').val();
     let id= $('#id').val();
     let title = $('#title').val();
     let annualSalary = $('#annualSalary').val();
-
+    //create opject from user input
     let newEmployee = {
         firstName: firstName,
         lastName: lastName,
@@ -22,7 +23,7 @@ function addEmployee(){
         title: title,
         annualSalary: annualSalary,
     };
-
+    //push object to array
     employeeList.push(newEmployee);
     $('#firstName').val('');
     $('#lastName').val('');
@@ -30,5 +31,30 @@ function addEmployee(){
     $('#title').val('');
     $('#annualSalary').val('');
 
-    console.log(employeeList);
+    displayEmployee();
 }
+
+function displayEmployee(){
+    //loop through employee list  - append each employee to DOM 
+    $('#empIn').empty();
+    for (employee of employeeList){
+        $('#empIn').append(
+            `<tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.id}</td>
+            <td>${employee.title}</td>
+            <td>${employee.annualSalary}</td>
+            <td><button>Delete</button></td>
+            </tr>`
+        );
+    }// end for loop
+}// end displayEmployee function
+
+function calculateTotal (){
+    //use totals from annual salaries to calculate a total
+}
+
+//add delete button
+//remove a line when delete button is used
+//
